e00d724abbbeb4c284f26c0c421aecbd218dd27d
fmanzanare <fernando_mzmt@hotmail.com>
2024-02-16 18:40:31 +0100

    front and back projects created

---
+++ tarea2AD/src/main/java/com/estech/tarea2AD/Tarea2AdApplication.java
@@ -0,0 +1,13 @@
+package com.estech.tarea2AD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea2AdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea2AdApplication.class, args);
+	}
+
+}
---
+++ tarea2AD/src/test/java/com/estech/tarea2AD/Tarea2AdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estech.tarea2AD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea2AdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
